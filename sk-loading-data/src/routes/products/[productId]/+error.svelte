0d49c0d5c35f73e23d5cb76de6761066fef5fe1a
Vishwas <vishwas.g5@gmail.com>
2023-01-29 20:12:38 +0530

    Errors and redirects

---
+++ sk-loading-data/src/routes/products/[productId]/+error.svelte
@@ -0,0 +1,6 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<h1>{$page.status}: {$page.error.message}</h1>
+<p>{$page.error.hint}</p>
--- sk-loading-data/src/routes/products/[productId]/+page.server.js
+++ sk-loading-data/src/routes/products/[productId]/+page.server.js
@@ -1,5 +1,12 @@
+import { error } from '@sveltejs/kit';
+// import { redirect } from '@sveltejs/kit';
+
 export const load = async ({ fetch, params }) => {
 	const { productId } = params;
+	if (productId > 3) {
+		throw error(404, { message: 'Product not found', hint: 'Try a different product' });
+		// throw redirect(307, '/products');
+	}
 	const title = 'Product details';
 	const notification = 'End of season sale! 60% off!';
 	const response = await fetch(`http://localhost:4000/products/${productId}`);
