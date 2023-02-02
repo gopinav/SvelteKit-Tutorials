6fe66a9f7cc54f538e0ef99cf3ac57ec71121f79
Vishwas <vishwas.g5@gmail.com>
2023-02-02 19:00:31 +0530

    Preload data

--- sk-loading-data/src/routes/+page.svelte
+++ sk-loading-data/src/routes/+page.svelte
@@ -1,3 +1,3 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<a href="/products">Products</a>
+<a href="/products" data-sveltekit-preload-data="off">Products</a>
--- sk-loading-data/src/routes/products/product.svelte
+++ sk-loading-data/src/routes/products/product.svelte
@@ -3,7 +3,9 @@
 </script>
 
 <div>
-	<h2>{product.title}</h2>
+	<a href={`/products/${product.id}`}>
+		<h2>{product.title}</h2>
+	</a>
 	<p>{product.description}</p>
 	<hr />
 </div>
