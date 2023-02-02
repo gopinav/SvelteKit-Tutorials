d34a31cfedc6585bdac49c5e62fdbc6439bbb56e
Vishwas <vishwas.g5@gmail.com>
2023-02-02 21:28:36 +0530

    Preload code

--- sk-loading-data/src/routes/+page.svelte
+++ sk-loading-data/src/routes/+page.svelte
@@ -1,3 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
+<h1 class="tall">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<a href="/products" data-sveltekit-preload-data="off">Products</a>
+<a href="/products" data-sveltekit-preload-code="viewport">Products</a>
+
+<!-- <style>
+	.tall {
+		height: 100vh;
+	}
+</style> -->
--- sk-loading-data/src/routes/products/+layout.svelte
+++ sk-loading-data/src/routes/products/+layout.svelte
@@ -20,6 +20,7 @@
 	.product-item {
 		display: inline-block;
 		width: 200px;
+		/* height: 100vh; */
 		margin: 10px;
 		border: 1px solid #ccc;
 		padding: 10px;
