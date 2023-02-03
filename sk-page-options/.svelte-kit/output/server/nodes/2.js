de65a76960f617f6232b23ce093a031ee23d5f67
Vishwas <vishwas.g5@gmail.com>
2023-02-03 19:16:04 +0530

    Delete page-options app

--- sk-page-options/.svelte-kit/output/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-export const component = async () => (await import('../entries/pages/_page.svelte.js')).default;
-export const file = '_app/immutable/components/pages/_page.svelte-443891f6.js';
-export const imports = ["_app/immutable/components/pages/_page.svelte-443891f6.js","_app/immutable/chunks/index-1664d19c.js"];
-export const stylesheets = [];
-export const fonts = [];
