26b0254042d3ba2b12d728aa37ad036b8c781e95
Vishwas <vishwas.g5@gmail.com>
2023-02-03 16:57:06 +0530

    Preload data

--- sk-loading-data/src/routes/+page.svelte
+++ sk-loading-data/src/routes/+page.svelte
@@ -1,6 +1,21 @@
+<script>
+	import { goto, preloadData } from '$app/navigation';
+</script>
+
 <h1 class="tall">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<a href="/products" data-sveltekit-preload-code="viewport">Products</a>
+<a href="/products" data-sveltekit-preload-data="hover">Products</a>
+<button
+	on:focus={() => {
+		preloadData('/products');
+	}}
+	on:mouseover={() => {
+		preloadData('/products');
+	}}
+	on:click={() => {
+		goto('/products');
+	}}>Go to products</button
+>
 
 <!-- <style>
 	.tall {
