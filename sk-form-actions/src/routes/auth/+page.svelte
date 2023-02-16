7962c160eb604fb72b14b84e6624c3cd5733b5a5
Vishwas <vishwas.g5@gmail.com>
2023-02-16 19:21:26 +0530

    Rename login to auth

---
+++ sk-form-actions/src/routes/auth/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	export let form;
+</script>
+
+<form method="post">
+	<p>{form?.message || ''}</p>
+	<input type="text" name="username" placeholder="Username" />
+	<input type="password" name="password" placeholder="Password" />
+	<button type="submit">Login</button>
+</form>
