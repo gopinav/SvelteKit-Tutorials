fa51245dd7fe46413e5c17b1915b6dffb1d34b59
Vishwas <vishwas.g5@gmail.com>
2023-02-17 18:01:13 +0530

    Handling redirections

--- sk-form-actions/src/routes/auth/+page.server.js
+++ sk-form-actions/src/routes/auth/+page.server.js
@@ -1,26 +1,24 @@
+import { fail, redirect } from '@sveltejs/kit';
+
 export const actions = {
-	login: async ({ request, cookies }) => {
+	login: async ({ request, cookies, url }) => {
 		const data = await request.formData();
 		const username = data.get('username');
 		const password = data.get('password');
 		if (!username || !password) {
-			return { message: 'Missing username or password' };
+			return fail(400, { username, message: 'Missing username or password' });
 		}
 		cookies.set('username', username, { path: '/' });
-		return {
-			message: 'Logged in'
-		};
+		throw redirect(303, url.searchParams.get('redirectTo') || '/');
 	},
-	register: async ({ request, cookies }) => {
+	register: async ({ request, cookies, url }) => {
 		const data = await request.formData();
 		const username = data.get('username');
 		const password = data.get('password');
 		if (!username || !password) {
-			return { message: 'Missing username or password' };
+			return fail(400, { username, message: 'Missing username or password' });
 		}
 		cookies.set('username', username, { path: '/' });
-		return {
-			message: 'Successfully registered'
-		};
+		throw redirect(303, url.searchParams.get('redirectTo') || '/');
 	}
 };
