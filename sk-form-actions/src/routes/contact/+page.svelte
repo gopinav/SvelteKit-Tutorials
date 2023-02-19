c14dcde778fa21056f22bc8fa1969f58edc6b5a6
Vishwas <vishwas.g5@gmail.com>
2023-02-19 11:24:37 +0530

    Snapshots

---
+++ sk-form-actions/src/routes/contact/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+	let name = '';
+	let email = '';
+	let message = '';
+
+	export const snapshot = {
+		capture: () => {
+			return {
+				name,
+				email,
+				message
+			};
+		},
+		restore: (data) => {
+			name = data.name;
+			email = data.email;
+			message = data.message;
+		}
+	};
+</script>
+
+<h1>Contact form</h1>
+
+<form method="post">
+	<div><input type="text" name="name" placeholder="Name" bind:value={name} /></div>
+	<div><input type="email" name="email" placeholder="Email" bind:value={email} /></div>
+	<div><textarea name="message" placeholder="Message" bind:value={message} /></div>
+	<button>Send</button> <a href="/">Cancel</a>
+</form>
